1847e3b0a5918310205e6b485639639f15a240c6
BianLee <leebrosjavaprogrammer@gmail.com>
2020-10-27 18:52:09 -0700

    pop up change

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+(function() {
+    var visited = localStorage.getItem('visited');
+    console.log(visited)
+    if (!visited) {
+      setTimeout(function() {
+        document.getElementById("popupID").style.visibility = "visible";
+        localStorage.setItem('visited', true);
+        console.log('it worked');
+      }, 1000);
+    }
+    if (visited) {
+        document.getElementById("popupID").style.visibility = "hidden";
+    }
+})();
+
+
 lightBoxClose = function() {
   document.querySelector(".popup").classList.add("closed");
 }
