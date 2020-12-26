290df6505a444ae1713fbd5f40a02f45de0012e6
BianLee <bianlee2004@gmail.com>
2020-12-26 00:00:23 -0800

    Oh, the weather outside is frightful

--- js/apply.js
+++ js/apply.js
@@ -1,14 +1,22 @@
-function openForm(evt, formName) {
-	var tabcontent, tablinks;
+function openForm(evt, formName, formNameTwo) {
+	var tabcontent, tablinks, tabcontentTwo;
+
+
 	tabcontent = document.getElementsByClassName("tabcontent");
+
 	for (i = 0; i < tabcontent.length; i++) {
 		tabcontent[i].style.display = "none";
 	}
+
+	console.log(tabcontent);
+
+
 	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
 	document.getElementById(formName).style.display = "block";
+		document.getElementById(formNameTwo).style.display = "block";
 	evt.currentTarget.className += " active";
 }
 
